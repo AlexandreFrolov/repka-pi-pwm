be2bf856de1327a64a8bc263db2fb8d93a187b63
Alexandre Frolov <alexandre@frolov.pp.ru>
2023-07-07 08:11:16 +0000

    Fix

--- sg90.py
+++ sg90.py
@@ -2,10 +2,18 @@ import RepkaPi.GPIO as GPIO
 from time import sleep
 import sys
 
-def setServoAngle(servo, angle):
+def setServoAngle(servo, frequency_Hz, angle):
     servo.start_pwm()  
     dutyCycle = angle / 18. + 3.
-    print(dutyCycle)
+    
+    print("Угол поворота: " + str(angle) + "\xb0")
+    print("Коэффициент заполнения Duty: " + str(dutyCycle) + "%")
+
+    period_duration = 1 / frequency_Hz * 1000
+    print("Длительность периода: ", period_duration, "мс, частота " + str(frequency_Hz) + " Гц")
+    
+    pulse_duration = (period_duration / 100) * dutyCycle
+    print("Длительность импульса ШИМ :", round(pulse_duration, 1), "мс")    
     
     servo.duty_cycle(dutyCycle)
     sleep(0.3)
@@ -19,6 +27,6 @@ if __name__ == "__main__":
     Duty_Cycle_Percent = 8
     servo = GPIO.PWM_A(PWM_chip, PWM_pin, frequency_Hz, Duty_Cycle_Percent)
 
-    setServoAngle(servo, int(sys.argv[1]))
+    setServoAngle(servo, frequency_Hz, int(sys.argv[1]))
     servo.pwm_close()
     del servo
